07469190e398c395c3a6727223e0d14d2656c592
PayelMajumder30 <payelajumder30@gmail.com>
2025-06-10 12:32:57 +0530

    migrate

--- resources/views/admin/user_management/userList.blade.php
+++ resources/views/admin/user_management/userList.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 
    @if(session('success'))
-      <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <div class="alert alert-success" role="alert">
         {{ session('success') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
