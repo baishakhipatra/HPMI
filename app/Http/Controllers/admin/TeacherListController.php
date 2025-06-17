45334b39908bb4b657e968955cbbe9493ecfd1c7
Payel Majumder <payelajumer30@gmail.com>
2025-06-17 22:11:35 +0530

    pull from main

--- app/Http/Controllers/admin/TeacherListController.php
+++ app/Http/Controllers/admin/TeacherListController.php
@@ -56,14 +56,11 @@ class TeacherListController extends Controller
             'qualifications'     => 'nullable|string|max:255',
             'address'            => 'nullable|string',
             'subjects_taught'    => 'nullable|array',
-            //'subjects_taught.*'  => 'nullable|exists:subjects,id',
             'subjects_taught.*'  => 'nullable|exists:class_wise_subjects,id',
             'classes_assigned'   => 'nullable|array',
             'classes_assigned.*' => 'nullable|exists:class_lists,id',
             'password'           => 'required|string|min:6',
         ]);
-        // dd($request->all());
-        //dd($request->all());
         // Custom DOB < DOJ validation
         $validator->after(function ($validator) use ($request) {
             if ($request->date_of_birth && $request->date_of_joining) {
@@ -72,7 +69,6 @@ class TeacherListController extends Controller
                 }
             }
         });
-        //dd($request->all());
         // Return with errors if any
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
@@ -125,9 +121,15 @@ class TeacherListController extends Controller
         $classLists = ClassList::all();
 
         $selectedClassIds = TeacherClass::where('teacher_id', $data->id)->pluck('class_id')->toArray();
-        $selectedSubjectIds = TeacherSubject::where('teacher_id', $data->id)->pluck('subject_id')->toArray();
+        $teacherSubjects = TeacherSubject::where('teacher_id', $data->id)->get();
+        $selectedClassWiseSubjectIds = [];
+        foreach($teacherSubjects as $teacherSubject) {
+            $selectedClassWiseSubjectIds[] = ClassWiseSubject::where('subject_id', $teacherSubject->subject_id)
+                      ->where('class_id', $teacherSubject->class_id)
+                      ->value('id');
+        }   
         $selectedClassIds = TeacherClass::where('teacher_id', $data->id)->pluck('class_id')->toArray();
-        return view('admin.teacher_management.edit', compact('data', 'selectedSubjectIds', 'selectedClassIds', 'classLists'));
+        return view('admin.teacher_management.edit', compact('data', 'selectedClassWiseSubjectIds', 'selectedClassIds', 'classLists'));
     }
 
     public function update(Request $request)
@@ -149,7 +151,7 @@ class TeacherListController extends Controller
             'subjects_taught'    => 'nullable|array',
             'subjects_taught.*'  => 'nullable|exists:class_wise_subjects,id',
             'classes_assigned'   => 'nullable|array',
-            'classes_assigned.*' => 'nullable|exists:class_wise_subjects,id',
+            'classes_assigned.*' => 'nullable|exists:class_lists,id',
         ]);
 
         // Custom DOB check
